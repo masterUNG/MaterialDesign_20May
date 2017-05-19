ed4ff2b997814ace7389066cbfac6d8929c0222c
masterUNG <phrombutr@gmail.com>
2017-05-20 02:30:37 +0700

    Initial commit

---
+++ app/src/main/java/appewtc/masterung/materialdesigntest1/MainActivity.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.materialdesigntest1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
